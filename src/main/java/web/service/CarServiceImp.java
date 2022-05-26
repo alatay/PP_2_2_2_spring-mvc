aee18e04a54d6a9806cc0022b50e4c0b534705cc
Natali Knyazeva <knyazeva14@mail.ru>
2022-05-26 19:12:46 +0700

    Create CarServiceImp.java

---
+++ src/main/java/web/service/CarServiceImp.java
@@ -0,0 +1,21 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.dao.CarDao;
+import web.Car;
+
+import java.util.List;
+
+@Component
+public class CarServiceImp implements CarService {
+
+    @Autowired
+    private CarDao carDao;
+
+
+    @Override
+    public List<Car> getCars(int count) {
+        return carDao.getCarList().subList(0, count);
+    }
+}
