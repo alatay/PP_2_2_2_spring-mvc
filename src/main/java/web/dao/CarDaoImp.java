a7c2f2466f4004c9fc7772484dd7503ddeb5fef4
Natali Knyazeva <knyazeva14@mail.ru>
2022-05-26 19:12:34 +0700

    Create CarDaoImp.java

---
+++ src/main/java/web/dao/CarDaoImp.java
@@ -0,0 +1,21 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDaoImp implements CarDao {
+    @Override
+    public List<Car> getCarList() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Cadillac", "Grey", 6));
+        carList.add(new Car("Chevrolet", "Orange", 4));
+        carList.add(new Car("BMW", "White", 14));
+        carList.add(new Car("Mercedes", "Black", 2));
+        carList.add(new Car("Ford", "Red", 1));
+        return carList;
+    }
+}
